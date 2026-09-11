7b917588a9fc929fe5e118f0869e7fe2a61b755b
Asada100 <107757703+Asada100@users.noreply.github.com>
2026-09-12 00:04:31 +0330

    Add reserved DragDrop Classification handler

---
+++ engine/activity/classificationTypes/dragDropClassification.js
@@ -0,0 +1,10 @@
+// Tahouri Edu Platform - DragDrop Classification
+// Reserved for a future version. Not registered in v1.0.
+(function (window) {
+    'use strict';
+    const DragDropClassification = {
+        prepare(content) { return content; },
+        classify() { throw new Error('DragDrop Classification is reserved for a future version'); }
+    };
+    window.DragDropClassification = DragDropClassification;
+})(window);
