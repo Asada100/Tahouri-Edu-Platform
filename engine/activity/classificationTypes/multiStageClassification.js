e389f0e165ed06d63bd1e3983ac757d0f8f055a9
Asada100 <107757703+Asada100@users.noreply.github.com>
2026-09-12 00:04:38 +0330

    Add reserved MultiStage Classification handler

---
+++ engine/activity/classificationTypes/multiStageClassification.js
@@ -0,0 +1,10 @@
+// Tahouri Edu Platform - MultiStage Classification
+// Reserved for a future version. Not registered in v1.0.
+(function (window) {
+    'use strict';
+    const MultiStageClassification = {
+        prepare(content) { return content; },
+        classify() { throw new Error('MultiStage Classification is reserved for a future version'); }
+    };
+    window.MultiStageClassification = MultiStageClassification;
+})(window);
